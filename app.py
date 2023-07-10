2d91312912266be6010c9223789c143466c28684
Fauzan Azhar <fauzan_azhar786@outlook.com>
2023-07-10 21:30:45 +0500

    Added Static graphs

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from dash import Dash, dcc, html, dash_table, callback, Input, Output
 import dash_bootstrap_components as dbc
 from dash_daq import BooleanSwitch
 import datetime as dt
+import plotly.express as px
+import plotly.graph_objects as go
 
 # Load data
 df = pd.read_csv('data.csv')
@@ -15,10 +17,35 @@ df["Date"] = pd.to_datetime(df["Date"], format="%d/%m/%Y").dt.date
 external_stylesheets = [dbc.themes.CERULEAN]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
+labels = df.columns[1:]
+values = [df[column].sum() for column in labels]
+pie = px.pie(df, values=values, names=labels, title='Visitors by Type')
+pie.update_traces(textposition='inside', textinfo='percent+label')
+
+lineChart = go.Figure()
+for column in labels:
+    lineChart.add_trace(go.Scatter(x=df['Date'], y=df[column],
+                    mode='lines+markers',
+                    name=column))
+lineChart.update_layout(title='FootTraffic in Each Category by Date', xaxis_title='Date', yaxis_title='Visits',
+                        width=1400, height=1000)
+
+df1 = df.copy()
+df1['Date'] = [date.strftime('%Y/%m') for date in df['Date']]
+df1 = df1.groupby(['Date']).sum().reset_index()
+
+barChart = px.bar(df1, x='Date', y=labels, title='FootTraffic in Each Category by Month')
+barChart.update_layout(xaxis_title='Month', yaxis_title='Visits')
 
 app.layout = html.Div(children=[
     html.Div(children=html.H1('State Bank of Pakistan - FootTraffic Dashboard',
                               style = {'text-align' : 'center'})),
+    html.Div(children = dcc.Graph(id='foottraffic-pie-graph', figure=pie), 
+             style={'width': '30%', 'vertical-align': 'top', "margin": "0px auto", "text-align" : 'center'}),
+    html.Div(children = dcc.Graph(id='foottraffic-lines-graph', figure=lineChart), 
+             style={'vertical-align': 'top', "margin": "0px auto", "text-align" : 'center'}),
+    html.Div(children = dcc.Graph(id='foottraffic-bar-graph', figure=barChart), 
+             style={'vertical-align': 'top', "margin": "0px auto", "text-align" : 'center'}),
     html.Div(children = [BooleanSwitch(id='toggle_view_table', on=True),
                         html.Label('Toggle View Table')],
                 style={'width': '20%', 'vertical-align': 'top', "margin": "0px auto", "text-align" : 'center'}),
@@ -42,4 +69,4 @@ def toggle_view_table(on):
         return {'display': 'none'}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run("0.0.0.0",8080)
