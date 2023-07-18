c46a1500f39ddb8f415dc419686041a328c697eb
Fauzan Azhar <fauzan_azhar786@outlook.com>
2023-07-18 08:39:56 +0500

    Updated Data Source

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 # Imports
+
+
 import pandas as pd
 from dash import Dash, dcc, html, dash_table, callback, Input, Output
 import dash_bootstrap_components as dbc
 from dash_daq import BooleanSwitch
-import datetime as dt
+from datetime import datetime
+import os
 import plotly.express as px
 import plotly.graph_objects as go
+import webbrowser
 
-# Load data
-df = pd.read_csv('data.csv')
+df = None
+# Load data from excel fil
+for file in os.listdir(os.path.abspath(os.path.join(os.getcwd(), 'data'))):
+    if file.endswith('.xlsx'):
+        if df is None:
+            df = pd.read_excel(os.path.abspath(os.path.join(os.getcwd(), 'data', file)), header=1, skiprows=1, skipfooter=1)
+            continue
+        temp = pd.read_excel(os.path.abspath(os.path.join(os.getcwd(), 'data', file)), header=1, skiprows=1, skipfooter=1)
+        df = pd.concat([df, temp], axis=0)
+
+
 
 # Convert date column to datetime
-df["Date"] = pd.to_datetime(df["Date"], format="%d/%m/%Y").dt.date
+df["DATE"] = [datetime.fromisoformat(str(date)).date() for date in df["DATE"]]
+df.drop(columns=['TOTAL DAY'], inplace=True)
+df.dropna(inplace=True)
+df["PURCHASE OF BOND"] = df["PURCHASE OF BOND"].astype(int)
+
 
 # Create Dash app
 external_stylesheets = [dbc.themes.CERULEAN]
@@ -24,39 +41,38 @@ pie.update_traces(textposition='inside', textinfo='percent+label')
 
 lineChart = go.Figure()
 for column in labels:
-    lineChart.add_trace(go.Scatter(x=df['Date'], y=df[column],
-                    mode='lines+markers',
-                    name=column))
+    lineChart.add_trace(go.Scatter(x=df['DATE'], y=df[column],
+                                   mode='lines+markers',
+                                   name=column))
 lineChart.update_layout(title='FootTraffic in Each Category by Date', xaxis_title='Date', yaxis_title='Visits',
                         width=1400, height=1000)
 
 df1 = df.copy()
-df1['Date'] = [date.strftime('%Y/%m') for date in df['Date']]
-df1 = df1.groupby(['Date']).sum().reset_index()
+df1['DATE'] = [date.strftime('%Y/%m') for date in df['DATE']]
+df1 = df1.groupby(['DATE']).sum().reset_index()
 
-barChart = px.bar(df1, x='Date', y=labels, title='FootTraffic in Each Category by Month')
+barChart = px.bar(df1, x='DATE', y=labels, title='FootTraffic in Each Category by Month')
 barChart.update_layout(xaxis_title='Month', yaxis_title='Visits')
 
 app.layout = html.Div(children=[
     html.Div(children=html.H1('State Bank of Pakistan - FootTraffic Dashboard',
-                              style = {'text-align' : 'center'})),
-    html.Div(children = dcc.Graph(id='foottraffic-pie-graph', figure=pie), 
-             style={'width': '30%', 'vertical-align': 'top', "margin": "0px auto", "text-align" : 'center'}),
-    html.Div(children = dcc.Graph(id='foottraffic-lines-graph', figure=lineChart), 
-             style={'vertical-align': 'top', "margin": "0px auto", "text-align" : 'center'}),
-    html.Div(children = dcc.Graph(id='foottraffic-bar-graph', figure=barChart), 
-             style={'vertical-align': 'top', "margin": "0px auto", "text-align" : 'center'}),
-    html.Div(children = [BooleanSwitch(id='toggle_view_table', on=True),
-                        html.Label('Toggle View Table')],
-                style={'width': '20%', 'vertical-align': 'top', "margin": "0px auto", "text-align" : 'center'}),
-    html.Div(children = [
-        html.Div(children = html.H4('FootTraffic Data')),
-        html.Div(children = dash_table.DataTable(data=df.to_dict('records'), page_size=30,
-                            style_cell={'text-align': 'center'})
-        )
-    ], style={'width': '50%', 'vertical-align': 'top', "margin": "0px auto", "text-align" : 'center'}, 
-    id = "DataTableDiv"),
+                              style={'text-align': 'center'})),
+    html.Div(children=[dcc.Graph(id='foottraffic-pie-graph', figure=pie),dcc.Graph(id='foottraffic-bar-graph', figure=barChart)],
+             style={'vertical-align': 'top', "margin": "0px auto", "text-align": 'center', 'display': 'flex'}),
+    html.Div(children=dcc.Graph(id='foottraffic-lines-graph', figure=lineChart),
+             style={'vertical-align': 'top', "margin": "0px auto", "text-align": 'center'}),
+    html.Div(children=[BooleanSwitch(id='toggle_view_table', on=False),
+                       html.Label('Toggle View Table')],
+             style={'width': '20%', 'vertical-align': 'top', "margin": "0px auto", "text-align": 'center'}),
+    html.Div(children=[
+        html.Div(children=html.H4('FootTraffic Data')),
+        html.Div(children=dash_table.DataTable(data=df.to_dict('records'), page_size=22,
+                                               style_cell={'text-align': 'center'})
+                 )
+    ], style={'width': '50%', 'vertical-align': 'top', "margin": "0px auto", "text-align": 'center'},
+        id="DataTableDiv"),
 ])
+
 
 @app.callback(
     Output(component_id='DataTableDiv', component_property='style'),
@@ -64,9 +80,12 @@ app.layout = html.Div(children=[
 )
 def toggle_view_table(on):
     if on:
-        return {'display': 'block','width': '50%', 'vertical-align': 'top', "margin": "0px auto", "text-align" : 'center'}
+        return {'display': 'block', 'width': '50%', 'vertical-align': 'top', "margin": "0px auto",
+                "text-align": 'center'}
     else:
         return {'display': 'none'}
 
+
 if __name__ == '__main__':
-    app.run("0.0.0.0",8080)
+    app.run("127.0.0.1", 8080, debug=False)
+    webbrowser.open("http://127.0.0.1:8080")
